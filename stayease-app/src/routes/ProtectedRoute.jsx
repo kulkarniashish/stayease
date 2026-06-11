7a3a79e5959050fc21e257aafa3ae20e9ae55f45
Jkishan <kjoshi@geturgently.com>
2026-06-11 13:48:04 +0530

    Role based flow improvement

--- stayease-app/src/routes/ProtectedRoute.jsx
+++ stayease-app/src/routes/ProtectedRoute.jsx
@@ -1,10 +1,17 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import CONSTANTS from "../utils/constants";
 
 export default function ProtectedRoute({ children }) {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, role } = useAuth();
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
+  }
+  if (role === CONSTANTS.ROLES.MANAGER) {
+    return <Navigate to="/manager" replace />;
+  }
+  if (role === CONSTANTS.ROLES.ADMIN) {
+    return <Navigate to="/admin/hotels" replace />;
   }
   return children;
 }
